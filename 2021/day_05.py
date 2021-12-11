a9b76c4ca57d9c169dbda8ee6adacb1807477252
TerraXF@gmail.com <Sonnikita2003>
2021-12-11 14:20:49 +0100

    day5 progress

--- 2021/day_05.py
+++ 2021/day_05.py
@@ -1,19 +1,25 @@
 def read_input(file):
     with open(file) as f:
-        line = f.read().split('\n')
-        coords = [cord.split(' -> ') for cord in line]
+        data =[]
+        line = f.read().strip().split('\n')
+        coords = [[p.split(',') for p in cord.split(' -> ')] for cord in line]
     return coords
 
 
 def solve(data):
-    return data
+        grid = [[0 for i in range(1000)]for j in range(1000)]
+        for point in data:
+            if point[0][0] == point[1][0]:
+
+
+    return grid
 
 
 def solve2(data):
     return data
 
 
-answer_1 = solve(read_input("input_klein_05.txt"))
+answer_1 = solve(read_input('input_klein_05.txt'))
 #answer_2 = solve2(*read_input("input_klein_05.txt"))
 
 
