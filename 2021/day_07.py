cee5395d40edb6195b3ce9a897b63fe50d0f6d0f
ReturntoSender <TerraXF@gmail.com>
2021-12-11 23:55:19 +0100

    Day7

--- 2021/day_07.py
+++ 2021/day_07.py
@@ -1,50 +1,38 @@
 def read_input(file):
     with open(file) as f:
         data = f.read().strip().split(',')
-        data = [int(fish) for fish in data]
+        data = [int(horizon) for horizon in data]
     return data
 
 
 def solve(data):
-    short_cycle = 18
-    long_cycle = 80
-    for i in range(long_cycle):
-        new_data = []
-        for fish in data:
-            if fish == 0:
-                fish = 6
-                data.append(9)
-            else:
-                fish -= 1
-            new_data.append(fish)
-        data = new_data
-
-    return len(data)
+    minimum = min(data)
+    maximum = max(data)
+    counter = []
+    for i in range(maximum-minimum):
+        count = 0
+        for height in data:
+            count += abs(maximum - i - height)
+        counter.append(count)
+    return min(counter)
 
 
 def solve2(data):
-    stats = []
-    short_cycle = 10
-    long_cycle = 256
-    for i in range(short_cycle):
-        new_data = []
-        for fish in data:
-            if fish == 0:
-                fish = 6
-                data.append(9)
-            else:
-                fish -= 1
-            new_data.append(fish)
-        data = new_data
-
-        stats.append(len(data))
-    for i in range(long_cycle - short_cycle):
-        stats.append(stats[-7] + stats[-9])
-    return stats[-1]
+    minimum = min(data)
+    maximum = max(data)
+    counter = []
+    for i in range(maximum-minimum):
+        count = 0
+        for height in data:
+            difference = abs(maximum - i - height)
+            fuel = int((difference * (difference + 1)) / 2)
+            count += fuel
+        counter.append(count)
+    return min(counter)
 
 
-answer_1 = solve(read_input('input_06.txt'))
-answer_2 = solve2(read_input("input_06.txt"))
+answer_1 = solve(read_input('input_07.txt'))
+answer_2 = solve2(read_input("input_07.txt"))
 
 
 print(f"Die Loesung 1: {answer_1}")
---
+++ 2021/day_08.py
@@ -0,0 +1,22 @@
+def read_input(file):
+    with open(file) as f:
+        data = f.read()
+    return data
+
+
+def solve(data):
+
+    return min(counter)
+
+
+def solve2(data):
+
+    return min(counter)
+
+
+answer_1 = solve(read_input('input_klein_08.txt'))
+#answer_2 = solve2(read_input("input_klein_08.txt"))
+
+
+print(f"Die Loesung 1: {answer_1}")
+#print(f"Die Loesung 2: {answer_2}")
