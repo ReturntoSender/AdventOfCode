c4c836706494786d6490f90702b9821d0972c8f3
ReturntoSender <TerraXF@gmail.com>
2021-12-12 17:36:47 +0100

    Day8 complete

--- 2021/day_08.py
+++ 2021/day_08.py
@@ -1,49 +1,41 @@
 def read_input(file):
     with open(file) as f:
-        data = f.read().strip().split('\n')
+        patterns, outputs = [], []
+        for row in f:
+            left, right = row.split('|')
+            patterns.append({len(x): set(x) for x in left.split()})
+            outputs.append([set(x) for x in right.split()])
 
-    return data
-
-
-def solve(data):
-
-    lines = []
-    digits = [2, 3, 4, 7]
-    counter = 0
-
-    for line in data:
-        line = (line.split(' | ')[1]).split()
-        lines.append(line)
-    for line in lines:
-        for digit in line:
-            if len(digit) in digits:
-                counter += 1
-
-    return counter
+    return patterns, outputs
 
 
-def sortLength(item):
-            return len(item)
+def solve(patterns, outputs):
+    return sum([len(n) in {2, 4, 3, 7} for row in outputs for n in row])
 
 
-def solve2(data):
-
-    lines = []
-    digit_list = []
-
-    for line in data:
-        wire = line.split(' ')[:10]
-        code = line.split(' ')[-4:]        
-        wire.sort(key = sortLength)
-        print(f'{wire} - {code}')
+def gen_sig():
+    example = 'abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg'.split()
+    example = [set(x) for x in example]
+    pattern = {len(x): x for x in example}
+    return {get_sig(x, pattern): str(i) for i, x in enumerate(example)}
 
 
-    return sum(digit_list)
+def get_sig(x, pattern):
+    a, b, c, d = len(x), x & pattern[2], x & pattern[3], x & pattern[4]
+    return a, len(b), len(c), len(d)
 
 
-#answer_1 = solve(read_input('input_08.txt'))
-answer_2 = solve2(read_input("input_klein_08.txt"))
+def solve2(patterns, outputs):
+    result = 0
+    signatures = gen_sig()
+    for pattern, output in zip(patterns, outputs):
+        result += int(''.join(signatures[get_sig(x, pattern)] for x in output))
+    return result
 
 
-#print(f"Die Loesung 1: {answer_1}")
+answer_1 = solve(*read_input('input_08.txt'))
+answer_2 = solve2(*read_input("input_08.txt"))
+
+
+print(f"Die Loesung 1: {answer_1}")
 print(f"Die Loesung 2: {answer_2}")
